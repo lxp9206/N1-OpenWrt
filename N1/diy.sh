f35b537475e079bb0fcedf8fd8a7afcdd4b8a962
lxp9206 <59569486+lxp9206@users.noreply.github.com>
2024-11-06 11:05:52 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -42,4 +42,4 @@ rm -rf feeds/luci/applications/luci-app-mosdns
 #rm -rf feeds/luci/applications/luci-app-design-config
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
